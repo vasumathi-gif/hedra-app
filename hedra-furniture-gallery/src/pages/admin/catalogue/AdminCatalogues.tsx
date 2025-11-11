817e3ac58a8b9a34ec00d1926e047597f87fc506
hemachandran-k <hemachandranh868@gmail.com>
2025-11-11 13:02:44 +0530

    feat: add AdminCatalogues Pages

--- hedra-furniture-gallery/src/pages/admin/catalogue/AdminCatalogues.tsx
+++ hedra-furniture-gallery/src/pages/admin/catalogue/AdminCatalogues.tsx
@@ -286,14 +286,7 @@ const handleOpenPdf = (c: Catalogue) => {
                         <Edit className="mr-2 h-4 w-4" />
                         Edit
                       </Button>
-                      <Button
-                        variant="outline"
-                        size="sm"
-                        onClick={() => handleView(c)}
-                        title="Open"
-                      >
-                        <Eye className="h-4 w-4" />
-                      </Button>
+                     
                       <Button
                         variant="outline"
                         size="sm"
